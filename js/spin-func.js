636e9a27e39429dfe3c474eda406e204d80d0d39
Nicholas-ua <m.savych@gmail.com>
2018-02-01 17:55:20 +0200

    Change method: mousemove

--- js/spin-func.js
+++ js/spin-func.js
@@ -8,7 +8,7 @@ $(function () {
     $("#product1, #product2, #product3").attr("src", arr[0]);
     //Click mode.
     $("#product1").threesixty({ images: arr,
-    method: 'click',
+    method: 'mousemove',
     direction: 'forward',
     sensibility: 2
     });
