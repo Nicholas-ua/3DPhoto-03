76b9e9f07b2e66fb5c819b1d11fd5caa5581cc13
Nicholas-ua <m.savych@gmail.com>
2018-02-01 17:11:21 +0200

    Add spin-func.js

---
+++ js/spin-func.js
@@ -0,0 +1,14 @@
+$(function () {
+    //Load the image URLs into an Array.
+    var arr = new Array();
+    $("#dvImages img").each(function () {
+    arr.push($(this).attr("src"));
+    });
+    //Set the first image URL as source for the Product.
+    $("#product1, #product2, #product3").attr("src", arr[0]);
+    //Click mode.
+    $("#product1").threesixty({ images: arr,
+    method: 'click',
+    sensibility: 2
+    });
+    });
